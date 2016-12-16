02b2d58e905fef68bbefdd0f50ee5af6fd202cd3
Schalaschaska <mihanhlystow@gmail.com>
2016-12-16 12:43:19 +0300

    Com 1

--- Meth_ferma/MainWindow.xaml.cs
+++ Meth_ferma/MainWindow.xaml.cs
@@ -24,5 +24,54 @@ namespace Meth_ferma
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Convert.ToInt32(textBox.Text);
+            double S = 0;
+            double y;
+            double k = 1;
+            double sqr_S = Math.Sqrt(S);
+            bool flag=true;
+            double pr_rez = 0;
+            bool prost = true;
+            double mn = 0;
+            double mn2 = 0;
+            
+
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+            }
+            if (prost)
+            {
+                MessageBox.Show("Число простое");
+            }
+            else
+            {
+               MessageBox.Show("Число не простое");
+            }
+         
+            Pr Ob = new Pr();
+            while(Ob.flag)
+            {
+                mn = Ob.Z(n);
+                mn2 = Ob.Z2(n);
+                
+            }
+            {
+
+            }
+            
+            label1.Content = "Разложение числа " + n + " было достигнуто за " + (Ob.k-1)+ " итераций\nПервый множитель " + Convert.ToString(Ob.Z(n)) + "\nВторой множитель " + Convert.ToString(Ob.Z2(n));
+        }
+
+
+        }
     }
-}
+
+
---
+++ Meth_ferma/Pr.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meth_ferma
+{
+    class Pr
+    {
+        public double S, y, sqr_S, k = 1, pr_rez, mn, mn2, K;
+        public bool flag = true;
+        public double Z(int N)
+        {
+            while (flag != false)
+            {
+                S = Math.Ceiling(Math.Sqrt(N));
+                y = Math.Pow((S + k), 2) - N;
+                sqr_S = Math.Sqrt(y);
+                k++;
+
+
+                if (sqr_S % 1 == 0)
+                {
+
+                    pr_rez = Math.Pow((S + k - 1), 2) - (sqr_S * sqr_S);
+                    flag = false;
+
+                    mn = (S + (k - 1) + sqr_S);
+                    mn2 = (S + (k - 1) - sqr_S);
+                }
+               
+            }
+           
+            return mn;
+
+        }
+        public double Z2(int N)
+        {
+            while (flag != false)
+            {
+                S = Math.Ceiling(Math.Sqrt(N));
+                y = Math.Pow((S + k), 2) - N;
+                sqr_S = Math.Sqrt(y);
+                k++;
+
+                if (sqr_S % 1 == 0)
+                {
+                    pr_rez = Math.Pow((S + k - 1), 2) - (sqr_S * sqr_S);
+                    flag = false;
+
+                    mn = (S + (k - 1) + sqr_S);
+                    mn2 = (S + (k - 1) - sqr_S);
+                }
+                
+            }
+            return mn2;
+
+        }
+
+      
+       
+    }
+    
+}
